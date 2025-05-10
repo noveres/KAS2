fe5ad535522a8d393d0ec0681214843cea4602c6
noveres <andrew901117@gmail.com>
2025-05-10 20:47:02 +0800

    整理組件，以及修復文字超出容器的問題

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SnavmenuComponent } from './snavmenu/snavmenu.component';
+import { SidebarNAVmenuDropDownComponent } from './layout/sidebar-navmenu-drop-down/sidebar-navmenu-drop-down.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,SnavmenuComponent],
+  imports: [RouterOutlet,SidebarNAVmenuDropDownComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
